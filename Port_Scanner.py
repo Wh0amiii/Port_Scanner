9846d3de815ced8c2ee741827e2e0175528d2b85
Crepso0 <65604658+Crepso0@users.noreply.github.com>
2022-01-25 21:31:25 +0100

    Add files via upload

---
+++ Port_Scanner.py
@@ -0,0 +1,33 @@
+import os
+from colorama import Fore
+import time
+
+green = Fore.LIGHTGREEN_EX
+red = Fore.RED
+
+print(Fore.MAGENTA + "'  ..____............_.....____..................................")
+print(Fore.MAGENTA + "'  .|.._.\.___.._.__|.|_../.___|..___.__._._.__.._.__...___._.__.")
+print(Fore.MAGENTA + "'  .|.|_)./._.\|.'__|.__|.\___.\./.__/._`.|.'_.\|.'_.\./._.\.'__|")
+print(Fore.MAGENTA + "'  .|..__/.(_).|.|..|.|_...___).|.(_|.(_|.|.|.|.|.|.|.|..__/.|...")
+print(Fore.MAGENTA + "'  .|_|...\___/|_|...\__|.|____/.\___\__,_|_|.|_|_|.|_|\___|_|...")
+print(Fore.MAGENTA + "'  ..............................................................")
+print(Fore.YELLOW + "                                           By Crepsoo#1034")
+print(" ")
+print("[1] Scan Ports")
+print("[2] Exit")
+print(" ")
+
+option = int(input(f"{green}[+]Choose an option (1-2): "))
+
+if option == 1:
+    ip = input(f"{green}[+]Writte target's ip: ")
+    ports = input(f"{green}[+]Writte the port range of your scanning process, example 0-80: ") 
+    print(f"{red}")
+    os.system(f"nmap -p {ports} -T5 -v -Pn {ip}")
+else:
+    print("Exiting...")
+    time.sleep(2)
+
+
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+Installing guide:
+
+1. -pip install -r requirements.txt
+
+Using guide:
+
+1. Choose [1] Scan Ports or [2] Exit
+
+2. If [1] is choosen, enter the ip's target, press enter
+
+3. Writte the port range of your scanning process, example: 20000-26000, press enter
+
+4. Wait a few time (it depends on how large is your port range)
+
+5. Ports will be written down were it says "PORT"
